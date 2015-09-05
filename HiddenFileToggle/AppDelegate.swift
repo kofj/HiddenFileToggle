c3c2e0c0de94d1b7a861c26148725db9790b2b2c
Frank Kung <kfanjian@gmail.com>
2015-09-05 16:08:11 +0800

    Initial Commit

---
+++ HiddenFileToggle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HiddenFileToggle
+//
+//  Created by 孔凡 on 15/9/5.
+//  Copyright (c) 2015年 Frank Kung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
